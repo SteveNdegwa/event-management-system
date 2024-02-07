d2f2b8969ece3da6a7bceca7cb91bb5b71cb9db8
Geekigen <kigencaleb50@gmail.com>
2024-02-07 15:36:28 +0300

    feat: add login and register user logic

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from user_management.models import CachedUserRequest
 
 # Register your models here.
+admin.site.register(CachedUserRequest)
--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+import ast
+import json
+from django.http import HttpResponse, JsonResponse
+import requests
+from django.views.decorators.csrf import csrf_exempt
+import os
+from dotenv import load_dotenv
 
-# Create your views here.
+from user_management.models import CachedUserRequest
+
+# Load environment variables from .env file
+load_dotenv()
+USER_MANAGEMENT_API = os.getenv('USER_MANAGEMENT_API')
+
+
+@csrf_exempt
+def login(request):
+    data = json.loads(request.body)
+    url = f"{USER_MANAGEMENT_API}/login/"
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print(response.json())
+        CachedUserRequest.objects.create(request_type='login', request_data=data, response_data=response.json(), is_successful=True)
+    else:
+        CachedUserRequest.objects.create(request_type='login', request_data=data, response_data=response.json(), is_successful=False)
+    return JsonResponse(response.json())
+
+
+@csrf_exempt
+def logout(request):
+    data = json.loads(request.body)
+    url = f"{USER_MANAGEMENT_API}/logout/"
+    response = requests.get(url, json=data)
+    if response.status_code == 200:
+        print(response.json())
+        CachedUserRequest.objects.create(request_type='logout', request_data={data}, response_data=response.json(), is_successful=True)
+    else:
+        CachedUserRequest.objects.create(request_type='logout', request_data={data}, response_data=response.json(), is_successful=False)
+    return JsonResponse(response.json())
+
+
+@csrf_exempt
+def register(request):
+    data = json.loads(request.body)
+    url = f"{USER_MANAGEMENT_API}/register/"
+    response = requests.post(url, json=data)
+    if response.status_code == 201:
+        CachedUserRequest.objects.create(request_type='register', request_data=data, response_data=response.json(), is_successful=True)
+    else:
+        CachedUserRequest.objects.create(request_type='register', request_data=data, response_data=response.json(), is_successful=False)
+    return JsonResponse(response.json())
+
+
+@csrf_exempt
+def verify_user(request):
+
+    url = f"{USER_MANAGEMENT_API}/"
+    response = requests.post(url)
+    return HttpResponse(response)
