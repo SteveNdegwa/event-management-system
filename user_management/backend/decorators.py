5511897cef5730d46b41a87610271c720b4f34a3
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-08 12:48:28 +0300

    feat: add verify_token decorator

---
+++ user_management/backend/decorators.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from functools import wraps
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+USER_MANAGEMENT_API = os.getenv('USER_MANAGEMENT_API')
+
+
+def verify_token(inner_function):
+    @wraps(inner_function)
+    def _wrapped_function(request, *args, **kwargs):
+        print("verifying token")
+        token = request.COOKIES.get('token')
+        print(token)
+        url = f"{USER_MANAGEMENT_API}/"
+        response = requests.post(url, json={"token": token})
+        print(response)
+        if response.ok:
+            return inner_function(request, *args, **kwargs)
+        return JsonResponse({"message": "Invalid Token"}, status=403)
+    return _wrapped_function
+
