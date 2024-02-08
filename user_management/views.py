51c2287150cd3a77e573499d27c28d46a8bf7aba
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-08 12:48:59 +0300

    feat: add verify_token decorator

--- user_management/views.py
+++ user_management/views.py
@@ -1,12 +1,11 @@
-import ast
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 import requests
 from django.views.decorators.csrf import csrf_exempt
+from .backend.decorators import verify_token
+from user_management.models import CachedUserRequest
 import os
 from dotenv import load_dotenv
-
-from user_management.models import CachedUserRequest
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,10 +13,12 @@ USER_MANAGEMENT_API = os.getenv('USER_MANAGEMENT_API')
 
 
 @csrf_exempt
+@verify_token
 def login(request):
     data = json.loads(request.body)
     url = f"{USER_MANAGEMENT_API}/login/"
     response = requests.post(url, json=data)
+    print(response.json())
     if response.status_code == 200:
         print(response.json())
         CachedUserRequest.objects.create(request_type='login', request_data=data, response_data=response.json(), is_successful=True)
@@ -29,13 +30,16 @@ def login(request):
 @csrf_exempt
 def logout(request):
     data = json.loads(request.body)
+    print(data)
     url = f"{USER_MANAGEMENT_API}/logout/"
-    response = requests.get(url, json=data)
+    print(url)
+    response = requests.post(url, json=data)
+    print(response)
     if response.status_code == 200:
         print(response.json())
-        CachedUserRequest.objects.create(request_type='logout', request_data={data}, response_data=response.json(), is_successful=True)
+        CachedUserRequest.objects.create(request_type='logout', request_data=data, response_data=response.json(), is_successful=True)
     else:
-        CachedUserRequest.objects.create(request_type='logout', request_data={data}, response_data=response.json(), is_successful=False)
+        CachedUserRequest.objects.create(request_type='logout', request_data=data, response_data=response.json(), is_successful=False)
     return JsonResponse(response.json())
 
 
@@ -56,4 +60,4 @@ def verify_user(request):
 
     url = f"{USER_MANAGEMENT_API}/"
     response = requests.post(url)
-    return HttpResponse(response)
+    return JsonResponse(response.json())
