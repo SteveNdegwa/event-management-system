f326cda1757cc9d6092d8aed6cb439c43ed64194
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-13 15:20:23 +0300

    feat: add try except statement

--- notifications/views.py
+++ notifications/views.py
@@ -3,16 +3,20 @@ from .backend.ServiceLayer import NotificationService
 
 
 def create_notification(user_id, name, description):
-    state = StateService().get(name="ongoing")
-    notification_to_save = NotificationService().create(
-        user_id=user_id,
-        name=name,
-        description=description,
-        notification_state=state,
-    )
+    try:
+        state = StateService().get(name="ongoing")
+        notification_to_save = NotificationService().create(
+            user_id=user_id,
+            name=name,
+            description=description,
+            notification_state=state,
+        )
 
-    # send notification and if successful
+        # send notification and if successful
 
-    new_state = StateService().get(name="completed")
-    NotificationService().update(notification_to_save.uuid, notification_state=new_state)
+        new_state = StateService().get(name="completed")
+        NotificationService().update(notification_to_save.uuid, notification_state=new_state)
+
+    except:
+        pass
 
