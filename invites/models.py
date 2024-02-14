58c4bc396e029dd460b0bd2a8a29f7bc49c70d70
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-14 08:03:19 +0300

    feat: add invite model

--- invites/models.py
+++ invites/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from base.models import GenericBaseModel, State
+from core.models import Event
+
 
 # Create your models here.
+
+class Invite(GenericBaseModel):
+    user_id = models.UUIDField()
+    target_email = models.EmailField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
