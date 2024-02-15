9f5dea2d538f339e2076adda3cd8a4ca877a4cd7
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-15 11:18:45 +0300

    feat: add try except block

--- services/request_processor.py
+++ services/request_processor.py
@@ -1,8 +1,17 @@
 import json
+from django.http import JsonResponse
 
 
 def get_request_data(request):
-    data = {}
-    # if request.method == 'POST':
-    data = json.loads(request.body)
-    return data
+    try:
+        # if request.method == 'POST':
+        data = json.loads(request.body)
+        user_id = request.COOKIES.get('user_id')
+        token = request.COOKIES.get('token')
+        if user_id:
+            data.user_id = user_id
+        if token:
+            data.token = token
+        return data
+    except:
+        return JsonResponse({"message": "Error retrieving request data", "code": "500"}, status=500)
