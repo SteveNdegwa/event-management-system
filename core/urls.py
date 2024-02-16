5ff1199d64c4d5cbb7af59ecd0900ce53525bfa8
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-16 16:50:34 +0300

    feat: add delete event endpoint

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("create/", views.create_event),
     path("update/", views.update_event),
+    path("delete/", views.delete_event),
     path("get/id/", views.get_event_by_id),
     path("get/", views.get_events),
     path("get/search/", views.search_events),
