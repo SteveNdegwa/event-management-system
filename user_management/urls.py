45332ab0def7d8c8082ca1af27cec3b7a85f5c71
SteveNdegwa <stevencallistus19@gmail.com>
2024-02-16 16:49:40 +0300

    feat: update forgot password and reset password endpoints

--- user_management/urls.py
+++ user_management/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', views.register),
     path('logout/', views.logout),
     path('confirm-email/', views.confirm_email),
-    path('forgot/', views.forgot_password),
-    path('reset/', views.reset_password),
+    path('forgot-password/', views.forgot_password),
+    path('reset-password/', views.reset_password),
     path('change-credentials/', views.change_credentials),
 ]
